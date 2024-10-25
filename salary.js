1e433b1164cd463571b068e9817d68a43b65ca42
DyadyaSerzh <62330623+DyadyaSerzh@users.noreply.github.com>
2024-10-26 00:44:55 +0300

    Update salary.js

--- salary.js
+++ salary.js
@@ -4,21 +4,22 @@ getRes.addEventListener('click', ()=>{
     let 
     timeNorm=+document.querySelector('.timeNorm').value,
     salary =+document.querySelector('.salary').value,
+    bonus =+document.querySelector('.bonus').value,
     dayNorm=+document.querySelector('.dayNorm').value,
     nightNorm=+document.querySelector('.nightNorm').value,
-    dayOvernorm=+document.querySelector('.dayOvernorm').value,
+    vacation=+document.querySelector('.vacation').value,
     resShow=document.querySelector('.resShow'),
     resDescr=document.querySelector('.resDescr');
-    let nightOvernorm=document.querySelector('.nightOvernorm').value;
+    let outWork=document.querySelector('.outWork').value;
 
     console.log('dayNorm====', dayNorm)
     console.log('timeNorm====',timeNorm)
-    let salaryOfHour=salary/timeNorm;
+    let salaryOfHour=salary/(timeNorm*11);
     console.log('salaryOfHour',salaryOfHour)
-    let workTime=(dayNorm + (nightNorm*12.4)+dayOvernorm*2+nightOvernorm*12.4*2)
-    console.log('dayNorm+(nightNorm*12.4)',(dayNorm+(nightNorm*12.4)))
+    let workTime=(dayNorm*11+vacation*11+outWork*5.5)
+    let NightOverPay=nightNorm*7*0.2*salaryOfHour
     console.log('workTime',workTime)
-    let res=workTime*salaryOfHour
+    let res=Math.trunc(workTime*salaryOfHour+NightOverPay+bonus)
     console.log('res',res)
     resShow.innerHTML=res
 
